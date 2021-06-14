73b76767e3a579601e87831cfdf3dcc742c19247
Michael Wu <maico910@gmail.com>
2021-06-14 15:41:31 -0300

    feat: create user

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import { v4 as uuid } from 'uuid'
 
 const app = express()
+
+app.use(express.json())
 
 interface User {
   id: string,
@@ -12,6 +15,16 @@ const users: User[] = []
 
 app.get('/users', (request, response) => {
   return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+  const {name, email} = request.body
+
+  const newUser: User = { id: uuid(), name, email}
+
+  users.push(newUser)
+
+  return response.json(newUser) 
 })
 
 app.listen('3333', () => {
