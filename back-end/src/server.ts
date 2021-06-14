03f84526fbee5427d30b78331a130fa779cf6e6c
Michael Wu <maico910@gmail.com>
2021-06-14 15:40:03 -0300

    feat: get list users

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,6 +2,18 @@ import express from 'express'
 
 const app = express()
 
+interface User {
+  id: string,
+  name: string,
+  email: string
+}
+
+const users: User[] = []
+
+app.get('/users', (request, response) => {
+  return response.json(users)
+})
+
 app.listen('3333', () => {
   console.log('Back-end started')
 })
