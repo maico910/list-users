aa0c16340f917752421fa5eaa564d11923495ec3
Michael Wu <maico910@gmail.com>
2021-06-14 17:29:34 -0300

    front: feat create Users

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="users">
+    <div class="container">
+      <section>
+        <h5 class="title">Novo usuário</h5>
+        <form @submit.prevent="createUser">
+          <input v-model="form.name" type="text" placeholder="Name" />
+          <input v-model="form.email" type="text" placeholder="E-mail" />
+          <button type="submit">Adicionar</button>
+        </form>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,6 +22,10 @@ export default defineComponent({
   data() {
     return {
       users: [] as User[],
+      form: {
+        name: '',
+        email: '',
+      },
     }
   },
 
@@ -28,17 +42,129 @@ export default defineComponent({
         console.warn(error)
       }
     },
+
+    async createUser() {
+      try {
+        let { data } = await axios.post('/users', this.form)
+
+        this.users.push(data)
+        this.form = {
+          name: '',
+          email: '',
+        }
+      } catch (error) {
+        console.warn(error)
+      }
+    },
+
   },
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  margin: 4rem auto;
+  max-width: 500px;
+  width: 90%;
+  display: grid;
+  grid-gap: 2.5rem;
+}
+
+.title {
+  font-size: 1rem;
+  font-weight: 500;
+  margin: 0.7rem 0;
+}
+
+form {
+  display: grid;
+  grid-gap: 1rem;
+}
+
+input {
+  background: transparent;
+  border: 1px solid #999fc6;
+  border-radius: 1rem;
+  padding: 0.6rem;
+  outline: none;
+  color: #e1e8ef;
+}
+
+input::placeholder {
+  color: #999fc6;
+}
+
+button {
+  background-color: #2d6cea;
+  color: #e1e8ef;
+  border: none;
+  border-radius: 1rem;
+  padding: 0.6rem 1.5rem;
+  width: max-content;
+  transition: all 0.3s linear;
+  outline: none;
+  cursor: pointer;
+  box-shadow: 0 0 5px 3px rgba(45, 108, 234, 0.3);
+}
+
+button:hover {
+  background-color: #1b5cdc;
+}
+
+p {
+  margin: 0;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+  display: grid;
+  grid-gap: 1rem;
+}
+
+li {
+  background-color: #2b3a4e;
+  padding: 1.2rem 1rem;
+  border-radius: 1rem;
+  position: relative;
+  list-style: none;
+  color: #8b98a8;
+}
+
+.destroy {
+  background-color: #d53e6b;
+  width: 24px;
+  height: 24px;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: all 0.2s linear;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 1.3rem;
+}
+
+.destroy:before,
+.destroy:after {
+  content: '';
+  width: 3px;
+  height: 13px;
+  background-color: #ececf6;
+  border-radius: 1rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+
+.destroy:before {
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+
+.destroy:after {
+  transform: translate(-50%, -50%) rotate(130deg);
+}
+
+.destroy:hover {
+  background-color: #984848;
 }
 </style>
